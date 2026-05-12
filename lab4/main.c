99f915157f439b3195538dd89832cce98a84a9a9
al3m1x <juliusz.radziszewski@gmail.com>
2026-05-12 18:41:14 +0200

    comments normalization

--- lab4/main.c
+++ lab4/main.c
@@ -5,8 +5,6 @@
 #include <omp.h>
 #include <math.h>
 
-// Usunięto numgen.c
-
 #define RANGESIZE 100000
 
 int main(int argc, char **argv) {
@@ -14,24 +12,20 @@ int main(int argc, char **argv) {
   Args ins__args;
   parseArgs(&ins__args, &argc, argv);
 
-  // set number of threads
+  // ilość wątków 
   omp_set_num_threads(ins__args.n_thr);
 
-  // Pobranie granic przedziału (analogicznie do Twojego kodu MPI)
+  // pobranie granic przedziału
   long long INITIAL_NUMBER = ins__args.start;
   long long FINAL_NUMBER = ins__args.stop;
 
   struct timeval ins__tstart, ins__tstop;
   gettimeofday(&ins__tstart, NULL);
 
-  // -------------------------------------------------------------------
-  // RUN YOUR COMPUTATIONS HERE (INCLUDING OPENMP STUFF)
-  // -------------------------------------------------------------------
-
   long long prime_count = 0;
   long long twin_prime_pairs = 0;
 
-  // 1. Sekwencyjne małe sito do sqrt(FINAL_NUMBER)
+  // 1. sekwencyjne małe sito do sqrt(FINAL_NUMBER)
   long long sqrt_n = (long long)sqrt(FINAL_NUMBER);
   char *small_sieve = (char *)calloc(sqrt_n + 1, sizeof(char));
 
@@ -43,35 +37,35 @@ int main(int argc, char **argv) {
       }
   }
 
-  // 2. Równoległe przetwarzanie porcjami (Segmented Sieve)
+  // 2. równoległe przetwarzanie porcjami (Segmented Sieve)
   #pragma omp parallel reduction(+:prime_count, twin_prime_pairs)
   {
-      // Prywatna tablica dla każdego wątku (+2 elementy na zakładkę dla liczb siostrzanych na krawędzi bloku)
+      // prywatna tablica dla każdego wątku (dodatkowe dwa elementy dla liczb siostrzanych na krawędzi bloku)
       char *mark = (char *)malloc((RANGESIZE + 2) * sizeof(char));
 
-      // Dynamiczny przydział bloków (chunków) do wątków
+      // dynamiczny przydział chunków do wątków
       #pragma omp for schedule(dynamic)
       for (long long low = INITIAL_NUMBER; low <= FINAL_NUMBER; low += RANGESIZE) {
           
           long long high = low + RANGESIZE - 1;
           if (high > FINAL_NUMBER) high = FINAL_NUMBER;
 
-          // Limit sprawdzania rozszerzony o 2, by łatwo wyłapać siostrzane na granicy
+          // limit sprawdzania rozszerzony o 2, żeby wyłapać siostrzane na granicy
           long long check_limit = (high + 2 > FINAL_NUMBER) ? FINAL_NUMBER : high + 2;
           long long size = check_limit - low + 1;
 
-          // Wyzerowanie prywatnej tablicy mark
+          // wyzerowanie prywatnej tablicy mark
           for(long long i = 0; i < size; i++) {
               mark[i] = 0;
           }
 
-          // Krawędziowy przypadek dla 0 i 1
+          // krawędziowy przypadek dla 0 i 1
           if (low <= 1) {
               if (low == 0) { mark[0] = 1; mark[1] = 1; }
               else if (low == 1) mark[0] = 1;
           }
 
-          // Wykreślanie wielokrotności w obecnym małym przedziale
+          // wykreślanie wielokrotności w obecnym małym przedziale
           for (long long p = 2; p <= sqrt_n; p++) {
               if (!small_sieve[p]) {
                   long long start = (low <= p) ? p * p : ((low + p - 1) / p) * p;
@@ -81,14 +75,14 @@ int main(int argc, char **argv) {
               }
           }
 
-          // Zliczanie w tym przedziale
+          // zliczanie w tym przedziale
           for (long long i = 0; i < (high - low + 1); i++) {
               long long current_num = low + i;
               
               if (!mark[i]) {
                   prime_count++;
                   
-                  // Sprawdzenie pary siostrzanej
+                  // sprawdzenie pary siostrzanej
                   if (current_num + 2 <= FINAL_NUMBER && !mark[i + 2]) {
                       twin_prime_pairs++;
                   }
@@ -96,22 +90,18 @@ int main(int argc, char **argv) {
           }
       }
 
-      // Zwolnienie prywatnej pamięci wątku na koniec sekcji równoległej
+      // zwolnienie prywatnej pamięci wątku
       free(mark);
   }
 
   free(small_sieve);
 
-  // Print results
+  // wyniki
   printf("Zakres: [%lld, %lld]\n", INITIAL_NUMBER, FINAL_NUMBER);
   printf("Total Primes Found: %lld\n", prime_count);
   printf("Total Twin Prime Pairs: %lld\n", twin_prime_pairs);
 
-  // -------------------------------------------------------------------
-  // END OF COMPUTATIONS
-  // -------------------------------------------------------------------
-
-  // synchronize/finalize your computations
+  // synchronizacja
   gettimeofday(&ins__tstop, NULL);
   ins__printtime(&ins__tstart, &ins__tstop, ins__args.marker);
 
